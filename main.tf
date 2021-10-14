6c1f167241af62ce2c7130dd654cd279cab47552
xulegion <73058380+xulegion@users.noreply.github.com>
2021-10-14 10:57:31 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,70 @@
+# 创建资源VPC
+resource "alicloud_vpc" "vpc" {
+  vpc_name   = "xxn_vpc"
+  cidr_block = "172.16.0.0/12"
+}
+
+# 创建资源switch并指定vpc_id
+resource "alicloud_vswitch" "vsw" {
+  vpc_id            = alicloud_vpc.vpc.id
+  cidr_block        = "172.16.0.0/21"
+  zone_id           = "cn-hangzhou-b"
+}
+
+# 负载均衡
+resource "alicloud_slb_load_balancer" "slb" {
+  load_balancer_name       = "xxn-slb-tf"
+  vswitch_id = alicloud_vswitch.vsw.id
+}
+
+
+# 创建资源安全组
+resource "alicloud_security_group" "group" {
+  name        = "xxn_security_group"
+  description = "安全组"
+  vpc_id      = alicloud_vpc.vpc.id
+}
+
+resource "alicloud_kms_key" "key" {
+  description            = "Hello KMS"
+  pending_window_in_days = "7"
+  key_state              = "Enabled"
+}
+
+# 创建实例
+resource "alicloud_instance" "instance" {
+
+  availability_zone = "cn-hangzhou-b"
+  security_groups   = alicloud_security_group.group.*.id
+
+  instance_type              = "ecs.n2.small"
+  number_of_instances         = var.instance_number 
+  system_disk_category       = "cloud_efficiency"
+  system_disk_name           = "xxn_system_disk"
+  system_disk_description    = "xxn_system_disk_description"
+  image_id                   = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
+  instance_name              = "xxn_instance"
+  vswitch_id                 = alicloud_vswitch.vsw.id
+  internet_max_bandwidth_out = 10     #出口带宽
+  data_disks {
+    name        = "xxn_data_disk"
+    size        = 20
+    category    = "cloud_efficiency"
+    description = "xxn_data_disk"
+    encrypted   = true
+    kms_key_id  = alicloud_kms_key.key.id
+  }
+}
+
+resource "alicloud_eip_address" "eip" {
+}
+
+resource "alicloud_eip_association" "eip_asso" {
+  allocation_id = alicloud_eip_address.eip.id
+  instance_id   = alicloud_instance.instance.*.id
+}
+
+output "eip" {
+  value = alicloud_eip_address.eip.public_ip
+}
 
